0d9e27332cd87c97fbaa986037bb4da76a3c5a1b
Anton Olsson <antonolsson.imp@gmail.com>
2024-04-06 17:35:00 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
 
-import "./App.css"
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+        </>
+    )
+)
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <div></div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <RouterProvider router={router} />
         </>
     )
 }
